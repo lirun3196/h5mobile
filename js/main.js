476eb57ea55b86ce9295c30683f16e8ef633a5f4
lirun3196 <lirun3196@163.com>
2015-10-26 08:27:39 +0800

    add info center

---
+++ js/main.js
@@ -0,0 +1,16 @@
+ function clearInput( id ){
+     if( id ){
+         var inputEle = document.getElementById( id );
+         var clearBtn = inputEle.nextSibling;
+         inputEle.onkeyup = function(){
+             var trimValue = document.getElementById( id).value.trim();
+             trimValue.length !== 0 ? clearBtn.style.display = 'inline-block' : clearBtn.style.display = 'none';
+         };
+         clearBtn.onclick = function(){
+             inputEle.value = '';
+             this.style.display = 'none';
+         }
+     }
+ }
+ var inputList = ['phone', 'password'];
+ inputList.forEach(clearInput);
