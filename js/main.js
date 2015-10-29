ecfa2e3b0d637dcba3f5e36e4d619240fd788062
lirun <lirun3196@163.com>
2015-10-29 20:26:01 +0800

    complete register

--- js/main.js
+++ js/main.js
@@ -1,20 +1,34 @@
- function clearInput( id ){
-     if( id ){
-         var inputEle = document.getElementById( id );
-         var clearBtn = inputEle.nextSibling;
-         inputEle.onkeyup = function(evt){
-             if( evt.keyCode ===8 ){
-                 var trimValue = document.getElementById( id).value.trim();
-                 trimValue.length !== 0 ? clearBtn.style.display = 'inline-block' : clearBtn.style.display = 'none';
-             }
-         };
-         clearBtn.onclick = function(){
-             inputEle.value = '';
-             inputEle.focus();
-             this.style.display = 'none';
-         }
-     }
- }
+function clearInput( id ){
+    if( id ){
+        var inputEle = document.getElementById( id );
+        var clearBtn = inputEle.nextSibling;
+        inputEle.onkeyup = function(evt){
+            if( evt.keyCode ===8 ){
+                var trimValue = document.getElementById( id).value.trim();
+                trimValue.length !== 0 ? clearBtn.style.display = 'inline-block' : clearBtn.style.display = 'none';
+            }
+        };
+        clearBtn.onclick = function(){
+            inputEle.value = '';
+            inputEle.focus();
+            this.style.display = 'none';
+        }
+    }
+}
 /*
  var inputList = ['phone', 'password'];
  inputList.forEach(clearInput);*/
+
+var yh = yh || {};
+ //蒙层
+yh.modal = function(id){
+    var selector = document.getElementById(id);
+    selector.style.display = 'block';
+    var closer = selector.querySelectorAll('.modal-close');
+    var closerLength = closer.length, i = 0;
+    for( i; i<closerLength; i++){
+        closer[i].onclick = function(){
+            selector.style.display = 'none';
+        }
+    }
+};
